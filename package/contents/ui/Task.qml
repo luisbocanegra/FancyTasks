luisbocanegra/FancyTasks

commit 7f99fe456d699a16a97560be8c453e78d12ab5ed

[taskmanager] Limit GroupDialog size, stop highlightwindow effect when it shown, increase items readability
REVIEW: 128038

Signed-off-by: Anthony Fieroni <bvbfan@abv.bg>

package/contents/ui/Task.qml
@@ -32,7 +32,7 @@ MouseArea {
     id: task
 
     width: groupDialog.mainItem.width
-    height: Math.max(theme.mSize(theme.defaultFont).height, units.iconSizes.small) + LayoutManager.verticalMargins()
+    height: Math.max(theme.mSize(theme.defaultFont).height, units.iconSizes.medium) + LayoutManager.verticalMargins()
 
     visible: false
 

package/contents/ui/main.qml
@@ -169,7 +169,7 @@ Item {
     TaskManagerApplet.Backend {
         id: backend
 
-        taskManagerItem: tasks
+        taskManagerItem: groupDialog.visible ? null : tasks
         toolTipItem: toolTipDelegate
         highlightWindows: plasmoid.configuration.highlightWindows
 

package/contents/ui/GroupDialog.qml
@@ -18,6 +18,7 @@
  ***************************************************************************/
 
 import QtQuick 2.0
+import QtQuick.Window 2.2
 
 import org.kde.plasma.core 2.0 as PlasmaCore
 import org.kde.draganddrop 2.0
@@ -32,6 +33,9 @@ PlasmaCore.Dialog {
     flags: Qt.WindowStaysOnTopHint
     hideOnWindowDeactivate: true
     location: plasmoid.location
+
+    property int preferredWidth: Screen.width / (3 * Screen.devicePixelRatio)
+    property int preferredHeight: Screen.height / (2 * Screen.devicePixelRatio)
 
     mainItem: Item {
         MouseHandler {
@@ -136,6 +140,9 @@ PlasmaCore.Dialog {
         } else {
             var task;
             var maxWidth = 0;
+            var maxHeight = 0;
+
+            backend.cancelHighlightWindows();
 
             for (var i = 0; i < taskList.children.length - 1; ++i) {
                 task = taskList.children[i];
@@ -147,11 +154,11 @@ PlasmaCore.Dialog {
                 task.textWidthChanged.connect(updateSize);
             }
 
-            maxWidth += LayoutManager.horizontalMargins() + units.iconSizes.small + 6;
+            maxWidth += LayoutManager.horizontalMargins() + units.iconSizes.medium + 2 * units.smallSpacing;
+            maxHeight = groupRepeater.count * (LayoutManager.verticalMargins() + Math.max(theme.mSize(theme.defaultFont).height, units.iconSizes.medium));
 
-            // TODO: Properly derive limits from work area size (screen size sans struts).
-            mainItem.width = Math.min(maxWidth, (tasks.vertical ? 640 - tasks.width : Math.max(tasks.width, 640)) - 20);
-            mainItem.height = groupRepeater.count * (LayoutManager.verticalMargins() + Math.max(theme.mSize(theme.defaultFont).height, units.iconSizes.small));
+            mainItem.height = Math.min(preferredHeight, maxHeight);
+            mainItem.width = Math.min(preferredWidth, (tasks.vertical ? Math.max(maxWidth, tasks.width) : Math.min(maxWidth, tasks.width)));
         }
     }
 }

package/contents/ui/ToolTipWindowMouseArea.qml
@@ -53,6 +53,9 @@ MouseArea {
         visible: parent.containsMouse && winId != 0
         tooltip: i18nc("close this window", "Close")
 
-        onClicked: tasksModel.requestClose(modelIndex);
+        onClicked: {
+            backend.cancelHighlightWindows();
+            tasksModel.requestClose(modelIndex);
+        }
     }
 }

commit 9a9918210f064795d516ee885bf750feae631639

Fix crash when invoking Present Windows with the group dialog open.
Summary:
This fixes a regression from 7f99fe45 which unsets the taskManagerItem
prop when the group dialog is shown to intentionally break window
highlighting while the dialog is open.

This patch instead fixes highlighting from the group dialog properly
by including it in the highlight list.

Also adds some extra crash guards.

CCMAIL:bvbfan@abv.bg
BUG:376205
BUG:376234

Reviewers: #plasma, anthonyfieroni

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D4525

package/contents/ui/Task.qml
@@ -142,6 +142,8 @@ MouseArea {
                     hideToolTipTemporarily();
                 }
             }
+
+            backend.cancelHighlightWindows();
         }
 
         pressed = false;

package/contents/ui/main.qml
@@ -175,8 +175,9 @@ Item {
     TaskManagerApplet.Backend {
         id: backend
 
-        taskManagerItem: groupDialog.visible ? null : tasks
+        taskManagerItem: tasks
         toolTipItem: toolTipDelegate
+        groupDialog: groupDialog
         highlightWindows: plasmoid.configuration.highlightWindows
 
         onAddLauncher: {