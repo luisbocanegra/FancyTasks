luisbocanegra/FancyTasks

commit 459ee17f8a3397d5a0f7aa158557203dcc837def

Compress calls to `updateSize`
Summary:
Repeater+Flow has no reliable way to transactionize/batch insertion,
e.g. `Flow.positionCompleted` is useless in a function that can change
the Flow's size, and transition events like populated and similar
aren't available. Refactoring this to be a ListView isn't appealing
because it would mean the code loses the property of being reused for
both the bar and the popup, and therefore is well-tested already.

This brings down the number of `updateSize` calls when opening the
dialog for a group of five from five to one.

BUG:400364

Reviewers: #plasma, davidedmundson

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D17006

package/contents/ui/GroupDialog.qml
@@ -127,7 +127,7 @@ PlasmaCore.Dialog {
 
                 onAnimatingChanged: {
                     if (!animating) {
-                        updateSize();
+                        Qt.callLater(updateSize);
                     }
                 }
 
@@ -146,11 +146,11 @@ PlasmaCore.Dialog {
                         return -1;
                     }
 
-                    onItemAdded: updateSize()
+                    onItemAdded: Qt.callLater(updateSize)
 
                     onItemRemoved: {
                         if (groupDialog.visible && index > 0 && index == count) {
-                            updateSize();
+                            Qt.callLater(updateSize);
                         }
                     }
                 }
@@ -262,7 +262,7 @@ PlasmaCore.Dialog {
         // Setting VisualDataModel.rootIndex drops groupRepeater.count to 0
         // before the actual row count. updateSize is therefore invoked twice;
         // only update size once the repeater count matches the model role.
-        } else if (!groupRepeater.aboutToPopulate || visualParent.childCount == groupRepeater.count) {
+        } else if (!groupRepeater.aboutToPopulate && visualParent.childCount == groupRepeater.count) {
             var task;
             var maxWidth = 0;
             var maxHeight = 0;

commit 96d02d6637d8b36e21e7889e1634061afd6a769f

Fix group popup dialog
Summary:
Fixes regression introduced with 459ee17f8a33: That diff contained
a change unrelated to the original submission, swapping a || out for
&& in response to a review note. This turns out to have broken things.

BUG:401508

Reviewers: davidedmundson

Subscribers: plasma-devel

Tags: #plasma

Differential Revision: https://phabricator.kde.org/D17219

package/contents/ui/GroupDialog.qml
@@ -262,7 +262,7 @@ PlasmaCore.Dialog {
         // Setting VisualDataModel.rootIndex drops groupRepeater.count to 0
         // before the actual row count. updateSize is therefore invoked twice;
         // only update size once the repeater count matches the model role.
-        } else if (!groupRepeater.aboutToPopulate && visualParent.childCount == groupRepeater.count) {
+        } else if (!groupRepeater.aboutToPopulate || visualParent.childCount == groupRepeater.count) {
             var task;
             var maxWidth = 0;
             var maxHeight = 0;